9594c6c66ba204b118825d218c2faa798715fc9c
killeraliens <killeraliens@outlook.com>
2020-05-17 13:03:22 -0600

    T package and io logic moved to controller

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
+require('dotenv').config()
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
+  TWIT_API_KEY: process.env.TWIT_API_KEY,
+  TWIT_API_SECRET_KEY: process.env.TWIT_API_SECRET_KEY
 }
