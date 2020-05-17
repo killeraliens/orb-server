d41fe53014fa05be489ccb68f7fc967cf1446e19
killeraliens <killeraliens@outlook.com>
2020-05-17 14:05:02 -0600

    removed old socket for clock and mathrandom

--- src/controller.js
+++ src/controller.js
@@ -55,32 +55,4 @@ module.exports = function(io) {
       socket.emit('allTweets', tweets)
     })
   }
-  // let interval;
-  // io.use((socket, next) => {
-  //   let symbol = socket.handshake.query.symbol;
-  //   if (symbol) {
-  //     console.log('symbol', symbol)
-  //     return next();
-  //   }
-  //   next()
-  // })
-
-  // io.on("connection", (socket) => {
-  //   console.log("New client connected");
-  //   if (interval) {
-  //     clearInterval(interval);
-  //   }
-  //   interval = setInterval(() => getApiAndEmit(socket), 1000);
-  //   socket.on("disconnect", () => {
-  //     console.log("Client disconnected");
-  //     clearInterval(interval);
-  //   });
-  // });
-
-  // const getApiAndEmit = socket => {
-  //   const response = new Date();
-  //   const mathRandom = Math.random()
-  //   socket.emit("get_date", response);
-  //   socket.emit("get_tweets", mathRandom)
-  // };
 }
