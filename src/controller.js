eccd05ec098259665778e252ffb5316f38453501
killeraliens <killeraliens@outlook.com>
2020-05-18 09:57:28 -0600

    stream persist outside connect

--- src/controller.js
+++ src/controller.js
@@ -4,6 +4,13 @@ const {
   TWIT_API_SECRET_KEY,
   TWIT_ACCESS_TOKEN,
   TWIT_ACCESS_TOKEN_SECRET } = require('./config')
+const T = new Twit({
+  consumer_key: TWIT_API_KEY,
+  consumer_secret: TWIT_API_SECRET_KEY,
+  access_token: TWIT_ACCESS_TOKEN,
+  access_token_secret: TWIT_ACCESS_TOKEN_SECRET
+})
+
 
 module.exports = function(io) {
   io.use((socket, next) => {
@@ -15,45 +22,47 @@ module.exports = function(io) {
     next()
   })
 
+  getStreamAndEmit(io)
   io.on("connection", (socket) => {
     console.log("Client connected")
-    getApiAndEmit(socket)
+    getInitalTweetsAndEmit(socket)
     socket.on("disconnect", () => {
       console.log("Client disconnected")
     });
   })
+}
 
-  function getApiAndEmit(socket) {
-    const T = new Twit({
-      consumer_key: TWIT_API_KEY,
-      consumer_secret: TWIT_API_SECRET_KEY,
-      access_token: TWIT_ACCESS_TOKEN,
-      access_token_secret: TWIT_ACCESS_TOKEN_SECRET
-    })
-
-    T.get(`search/tweets`, { q: '#corona', count: 10 }, (err, data, response) => {
-      const tweets = []
-      for (let i = 0; i < data.statuses.length; i++) {
-        const tweet = data.statuses[i]
-        let tweetBody = {
-          text: tweet.text,
-          userScreenName: '@' + tweet.user.screen_name,
-          userImage: tweet.user.profile_image_url_https,
-          userDescription: tweet.user.description
-        }
-        try {
-          if (tweet.entities.media[0].media_url_https) {
-            tweetBody.image = tweet.entities.media[0].media_url_https
-          }
-        } catch (err) { }
-        tweets.push(tweetBody)
+function getInitalTweetsAndEmit(socket) {
+  T.get(`search/tweets`, { q: '#corona', count: 10 }, (err, data, response) => {
+    const tweets = []
+    for (let i = 0; i < data.statuses.length; i++) {
+      const tweet = data.statuses[i]
+      let tweetBody = {
+        text: tweet.text,
+        userScreenName: '@' + tweet.user.screen_name,
+        userImage: tweet.user.profile_image_url_https,
+        userDescription: tweet.user.description
       }
-      socket.emit('allTweets', tweets)
-    })
+      try {
+        if (tweet.entities.media[0].media_url_https) {
+          tweetBody.image = tweet.entities.media[0].media_url_https
+        }
+      } catch (err) { }
+      tweets.push(tweetBody)
+    }
+    socket.emit('allTweets', tweets)
+  })
+}
 
-    T.stream(`statuses/filter`, { track: '#corona' }).on('tweet', (tweet) => {
-      console.log('NEWW TWEET', tweet.user.screen_name)
-      socket.emit('tweet', { ...tweet} )
-    })
-  }
+function getStreamAndEmit(socket) {
+  T.stream(`statuses/filter`, { track: '#corona' }).on('tweet', (tweet) => {
+    let tweetBody = {
+      text: tweet.text,
+      userScreenName: '@' + tweet.user.screen_name,
+      userImage: tweet.user.profile_image_url_https,
+      userDescription: tweet.user.description
+    }
+    console.log('NEWW TWEET', tweetBody.userScreenName)
+    socket.emit('tweet', { ...tweetBody} )
+  })
 }
