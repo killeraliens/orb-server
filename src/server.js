54c71b62def2f55910552b57916647bcc80b0d21
killeraliens <killeraliens@outlook.com>
2020-05-19 07:58:20 -0600

    query

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = require('./app')
 const http = require('http')
 const server = http.createServer(app)
 const io = require('socket.io')(server)
-require('./controller')(io)
+require('./controller')(app, io)
 
 server.listen(PORT, () => { console.log(`listening on port ${PORT}`) })
 
